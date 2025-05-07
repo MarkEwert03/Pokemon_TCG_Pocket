16caa9adf04e2a7401c2556e15f23b8b96ea4c65
MarkEwert03 <mewert03@gmail.com>
2025-05-07 13:43:47 -0700

    Use pathlib to handle relative and absolute paths

--- tcg_extract/driver.py
+++ tcg_extract/driver.py
@@ -1,15 +1,11 @@
-import os
 from bs4 import BeautifulSoup
+from pathlib import Path
 from parser import extract_card
 from writer import write_to_csv
 
 
 # Raw data found at
 # https://game8.co/games/Pokemon-TCG-Pocket/archives/482685
-
-# Input and output file paths
-input_file = "data/raw/medium.html"
-output_file = "data/processed/medium.csv"
 
 
 def main():
@@ -19,20 +15,28 @@ def main():
     This function:
     - Opens the raw HTML file
     - Parses it with BeautifulSoup
-    - Extracts one row from a table
+    - Extracts rows from the main table
     - Converts it to a structured dictionary
     - Writes it to a CSV file
     """
-    # Check path exists
-    if not os.path.isfile(input_file):
-        raise FileNotFoundError(f"Input file '{input_file}' does not exist.")
+    # Define project root as two levels up from this file
+    driver_file = Path(__file__).resolve()
+    PROJ_ROOT = driver_file.parent.parent
+    # Define input/output folders using absolute paths
+    raw_dir = PROJ_ROOT / "data" / "raw"
+    processed_dir = PROJ_ROOT / "data" / "processed"
+    # Choose input file and generate matching output file path
+    input_file = raw_dir / "small.html"
+    output_file = processed_dir / input_file.with_suffix(".csv").name
+
+    # Validate input and create output dir if needed
+    if not input_file.is_file():
+        raise FileNotFoundError(f"Input file not found: {input_file}")
+    processed_dir.mkdir(parents=True, exist_ok=True)
 
     # Load and parse the raw HTML file
     with open(input_file, "r", encoding="utf-8") as file:
         soup = BeautifulSoup(file, "lxml")
-
-    # Create list to store dict of cleaned card data
-    cards_data = []
 
     # Extract the main table from the HTML file
     table = soup.find("table", class_="a-table")
@@ -44,6 +48,9 @@ def main():
     if not cards_html:
         raise ValueError(f"No <tr> elements found int <tbody> {input_file}")
 
+    # Create list to store dict of cleaned card data
+    cards_data = []
+
     # Iterate over each <tr> element representing all metadata for one card
     for card_html in cards_html:
         row = extract_card(card_html)
@@ -52,7 +59,7 @@ def main():
     # Export the parsed data to CSV
     write_to_csv(cards_data, output_file)
 
-    print(f"CSV file '{output_file}' created successfully!")
+    print(f"CSV file created: {output_file.relative_to(PROJ_ROOT)}")
 
 
 if __name__ == "__main__":

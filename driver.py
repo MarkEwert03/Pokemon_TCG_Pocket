61ff3a2de44579e8c0fe3b93ae6a841f7354a267
MarkEwert03 <mewert03@gmail.com>
2025-05-07 12:55:07 -0700

    Add input path error checking

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import bs4
 
 # Input and output file paths
@@ -95,7 +96,7 @@ def write_to_csv(cards_data: list[dict[str, str]]):
     """
     # Use the first entry to get the column names
     column_names = cards_data[0].keys()
-    
+
     # Iterate through list and write each dict as a new row
     with open(output_file, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=column_names)
@@ -115,7 +116,11 @@ def main():
     - Converts it to a structured dictionary
     - Writes it to a CSV file
     """
-     # Load and parse the raw HTML file
+    # Check path exists
+    if not os.path.isfile(input_file):
+        raise FileNotFoundError(f"Input file '{input_file}' does not exist.")
+
+    # Load and parse the raw HTML file
     with open(input_file, "r", encoding="utf-8") as file:
         soup = bs4.BeautifulSoup(file, "lxml")
 
@@ -126,12 +131,12 @@ def main():
     table = soup.find("table", class_="a-table")
     if table is None:
         raise ValueError(f"Table with class 'a-table' not found in {input_file}")
-    
+
     # Extract all <tr> elements of the <tbody>
     cards_html = table.find("tbody").find_all("tr")
-    if not card_html:
+    if not cards_html:
         raise ValueError(f"No <tr> elements found int <tbody> {input_file}")
-    
+
     # Iterate over each <tr> element representing all metadata for one card
     for card_html in cards_html:
         row = extract_card(card_html)

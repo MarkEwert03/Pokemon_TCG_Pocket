e71c21ecf161cc5c7204cdd2301dada82f08ee50
MarkEwert03 <mewert03@gmail.com>
2025-05-07 10:57:10 -0700

    Add basic extraction of HTML data

--- driver.py
+++ driver.py
@@ -1,5 +1,20 @@
+from bs4 import BeautifulSoup
+
+input_file = "data/raw/single.html"
+
 def main():
-    print("Driver")
+    """Run the main function."""
+    with open(input_file, "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "lxml")
+        
+    table = soup.find("tr")
+    cells = table.find_all("td")
+    
+    cards_data = []
+    for cell in cells:
+        cards_data.append(cell.text.strip())
+        
+    print(cards_data)
     
 if __name__ == "__main__":
     main()
